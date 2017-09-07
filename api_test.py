ec1080991f1b52d6da47db566b9959bc60bd6992
apo <apostoae.andrei@gmail.com>
2017-09-08 00:56:07 +0300

    trained all models

--- api_test.py
+++ api_test.py
@@ -7,13 +7,20 @@ import time
 
 
 def pretrain():
-    with open('pretrained/results.csv', 'w+') as results_file:
-        results_file.write('mmr,train_size,test_size,roc_auc\n')
+    # with open('pretrained/results.csv', 'w+') as results_file:
+    #     results_file.write('mmr,train_size,test_size,roc_auc\n')
 
-    for i in range(5):
-        mmr_low = 2000 + i * 100 - 200
-        mmr_base = 2000 + i * 100
-        mmr_high = 2000 + i * 100 + 200
+    for i in range(31):
+        if i < 20:
+            offset = 200
+        elif i < 25:
+            offset = 250
+        else:
+            offset = 300
+
+        mmr_low = 2000 + 100 * i - offset
+        mmr_base = 2000 + 100 * i
+        mmr_high = 2000 + 100 * i + offset
 
         train_features, _ = read_dataset('706e_train_dataset.csv', low_mmr=mmr_low, high_mmr=mmr_high)
         test_features, _ = read_dataset('706e_test_dataset.csv', low_mmr=mmr_low, high_mmr=mmr_high)
@@ -29,15 +36,18 @@ def pretrain():
 
 def main():
 
-    synergies = np.loadtxt('pretrained/synergies_all.csv')
-    counters = np.loadtxt('pretrained/counters_all.csv')
-    similarities = np.loadtxt('pretrained/similarities_all.csv')
+    # synergies = np.loadtxt('pretrained/synergies_all.csv')
+    # counters = np.loadtxt('pretrained/counters_all.csv')
+    # similarities = np.loadtxt('pretrained/similarities_all.csv')
+    #
+    # start_time = time.time()
+    # query(3011, [59, 56, 54, 48, 34], [40, 41, 52, 68], synergies, counters, similarities)
+    # end_time = time.time()
+    #
+    # print (end_time - start_time) * 1000, 'ms'
 
-    start_time = time.time()
-    query(3011, [59, 56, 54, 48, 34], [40, 41, 52, 68], synergies, counters, similarities)
-    end_time = time.time()
+    pretrain()
 
-    print (end_time - start_time) * 1000, 'ms'
 
 if __name__ == '__main__':
     main()
